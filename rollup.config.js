c03ea738467cb76ab1f4bb2e1ab65b62ef05a4d6
john gravois <jagravois@gmail.com>
2020-04-02 17:21:44 -0700

    2020 refresh

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { rollup } from 'rollup';
-import json from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
 
 var pkg = require('./package.json');
@@ -11,19 +11,19 @@ export default {
   format: 'umd',
   external: ['leaflet'],
   plugins: [
-    nodeResolve({
-      jsnext: true,
-      main: false,
-      browser: false,
-      extensions: [ '.js', '.json' ]
-    }),
+    nodeResolve(),
     json(),
     uglify()
   ],
-  dest: 'built/bootstrap-geocoder.js',
-  globals: {
-    'leaflet': 'L',
-    'esri-leaflet': 'L.esri'
-  },
-  sourceMap: 'inline'
+  input: 'src/GeosearchInput.js',
+  output: {
+    file: 'built/bootstrap-geocoder.js',
+    globals: {
+      'leaflet': 'L',
+      'esri-leaflet': 'L.esri'
+    },
+    sourceMap: true,
+    format: 'umd',
+    name: 'L.esri.BootstrapGeocoder'
+  }
 }
